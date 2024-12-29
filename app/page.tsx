439dc486342495551dabdc5fb3282f3e2167b0bb
davit1801 <155184127+davit1801@users.noreply.github.com>
2024-12-29 23:11:29 +0400

    complete 9-16 chapters

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+// import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-const shape = styles.shape;
+// const shape = styles.shape;
 
 export default function Page() {
   return (
